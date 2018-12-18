7130d8fec4ce8efc0de007f8c0e4929f00a82285
Muhammad Sadiq Mustawin <39640899+16110318@users.noreply.github.com>
2018-12-19 02:22:57 +0700

    masukin data productku.php
    masukin data productku.php

--- admin/header admin.php
+++ admin/header admin.php
@@ -1,7 +1,6 @@
-<?php include '../system/koneksi.php' ?>
 <header>
-      <nav class="navbar nav-tabs navbar-expand-md navbar-light fixed-top bg-light">
-        <a class="navbar-brand" href="index.php"><img src="../img/logofont.svg" width="150" height="auto" class="d-inline-block align-top" alt=""></a>
+      <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
+        <a class="navbar-brand" href="#"><img src="img/logofont.svg" width="150" height="auto" class="d-inline-block align-top" alt=""></a>
           <!--<img src="img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">-->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -9,29 +8,21 @@
         <div class="collapse navbar-collapse" id="navbarCollapse">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="product.php">Product</a>
+              <a class="nav-link" href="#">Product</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="user.php">User</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="message.php">Message</a>
+              <a class="nav-link" href="quality.html">Quality</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Contact Us</a>
             </li>
           </ul>
-             <?php
-   $query = mysqli_query($con,"SELECT * FROM admin");
-   $record = mysqli_fetch_array($query);
-   
-   ?>
              <div class="dropdown">
-            USER - <?php echo $record['nama_admin']; ?>
-          <button class="dropbtn"><img src="../img/user2.png"></button>
+            ADMIN - Ray Reysandi  
+          <button class="dropbtn"><img src="user2.png"></button>
           <div class="dropdown-content">
             <a href="indexlogin.html"> Profile </a>
             <a href="history payment.html"> History </a>
